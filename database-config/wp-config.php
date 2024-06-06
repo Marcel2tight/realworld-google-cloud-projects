bf4ec4166078f94a62a76972e90a439d0a8e7afd
Marcel2tight <159199520+Marcel2tight@users.noreply.github.com>
2024-06-05 22:50:40 -0700

    Update wp-config.php

--- database-config/wp-config.php
+++ database-config/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'admin' );
 
 /** Database hostname */
-define( 'DB_HOST', '34.29.173.104' );
+define( 'DB_HOST', '34.118.240.120' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
